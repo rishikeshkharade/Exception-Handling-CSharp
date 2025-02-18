653e8c8c0b986b608e1ae3c475b2f4c86d56b5b9
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-18 23:27:01 +0530

    use of try catch finally

---
+++ UsingTryCatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class UsingTryCatch
+    {
+
+
+        public static void TryCatchDemo()
+        {
+            try
+            {
+
+                Console.WriteLine("Enter a number: ");
+
+                var num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Square of {num} is {num * num}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error info:" + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Re-try with a different number.");
+            }
+        }
+    }
+}
---
+++ UsingTryCatchFinally.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class UsingTryCatchFinally
+    {
+        public static void TryCatchFinallyDemo()
+        {
+            try
+            {
+
+                Console.WriteLine("Enter a number: ");
+
+                var num = int.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Sqaure of {num} is {num * num}");
+            }
+            catch
+            {
+                Console.WriteLine("Error occured.");
+
+            }
+
+            finally
+            {
+                Console.WriteLine("Re-try with a different number.");
+            }
+        }
+    }
+}
