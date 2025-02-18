1dc7cd2caaa181af663a1ae3c49e36b2dc7f320a
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-18 23:18:17 +0530

    Welcome to Exception Handling Problems

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Exception_Handling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //UsingTryCatchFinally.TryCatchFinallyDemo();
+        // UsingTryCatch.TryCatchDemo();
+        //ExceptionFilters.MultipleCatchBlock();
+        //FinallyBlock.FinallyBlockMethod();
+        //Nestedtry_catch.Nestedtrycatch();
+        //ThrowKeyword.throwkeyword();
+        //Re_throwingfanException.Method();
+        ThrowCustomException.MainMethod();
+
+    }
+}
