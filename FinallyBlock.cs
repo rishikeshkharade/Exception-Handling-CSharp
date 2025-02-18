214a7b9cb0781e5306a7b321c4aa080744ec5177
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-18 23:23:43 +0530

    How to use finally Block

---
+++ FinallyBlock.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class FinallyBlock
+    {
+        public static void FinallyBlockMethod()
+        {
+            FileInfo file = null;
+
+            try
+            {
+                Console.Write("Enter a file name to write: ");
+                string fileName = Console.ReadLine();
+                file = new FileInfo(fileName);
+                using (StreamWriter writer = file.AppendText())
+                {
+                    writer.WriteLine("Hello World!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occured: {0}", ex.ToString());
+            }
+            finally
+            {
+
+                file = null;
+            }
+        }
+    }
+    }
