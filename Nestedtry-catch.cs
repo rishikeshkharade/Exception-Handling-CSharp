aa2632631a5ab8d8cad3d1678b70f1e97adae448
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-18 23:22:46 +0530

    How to do Nested try catch Block

---
+++ Nestedtry-catch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class Nestedtry_catch
+    {
+        public static void Nestedtrycatch()
+        {
+            var divider = 0;
+
+            try
+            {
+                try
+                {
+                    var result = 100 / divider;
+                }
+                catch(NullReferenceException ex)
+                {
+                    Console.WriteLine("Inner catch");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Outer catch");
+            }
+            }
+            } 
+    }
