9d79b04a78da5642d80f3390e892e750418410b9
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-18 23:19:31 +0530

    Custom Exception

---
+++ ApplicationException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+    }
+
+    [Serializable]
+    class InvalidStudentNameException : Exception
+    {
+        public InvalidStudentNameException() { }
+
+        public InvalidStudentNameException(string name) : base(String.Format("Invalid Student Name: {0}", name))
+        {
+        }
+    }
+}
---
+++ ThrowCustomException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class ThrowCustomException
+    {
+        public static void MainMethod()
+        {
+            Student newStudent = null;
+
+            try
+            {
+                newStudent = new Student();
+                newStudent.StudentName = "Rishikesh007";
+
+                ValidateStudent(newStudent);
+            }
+            catch (InvalidStudentNameException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void ValidateStudent(Student student)
+        {
+            Regex regex = new Regex("^[a-zA-Z]+$");
+
+            if (!regex.IsMatch(student.StudentName))
+            {
+                throw new InvalidStudentNameException(student.StudentName);
+            }
+        }
+    }
+}
