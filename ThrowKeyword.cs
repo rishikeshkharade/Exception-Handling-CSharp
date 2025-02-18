b62fd157c8dcbc8edb35277e8d39c1a52e6fab95
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-18 23:21:42 +0530

    Use of throw keyword

---
+++ ThrowKeyword.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    internal class ThrowKeyword
+    {
+        public static void throwkeyword()
+        {
+            object student = null;
+
+            try
+            {
+                PrintStudentName(student);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+        private static void PrintStudentName(object std)
+        {
+            if (std == null) {
+                throw new NullReferenceException("Student object is null.");
+
+                Console.WriteLine(std);
+            }
+        }
+    }
+}
