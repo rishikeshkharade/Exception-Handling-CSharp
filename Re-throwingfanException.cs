b35e9ae4708eaf2ba0e6c2b66005d9524a7cbdf4
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-18 23:20:48 +0530

    Re-throwing an Exception

---
+++ Re-throwingfanException.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    public class Re_throwingfanException
+    {
+        public static void Method()
+        {
+            try
+            {
+                Method1();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        static void Method1()
+        {
+            try
+            {
+                Method2();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        static void Method2()
+        {
+                
+           string str = null;
+            try
+            {
+                Console.WriteLine(str[0]);
+            }
+            catch(Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
